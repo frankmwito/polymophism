6376ef6fedba6cf1682a0315243ca1c1f5903189
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-21 21:29:33 +0300

    Polymorphic class with inheritance

--- Polymophic function.py
+++ Polymophic function.py
@@ -1,4 +1,8 @@
 # polymorphic function
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
 import math
 
 class Circle:
