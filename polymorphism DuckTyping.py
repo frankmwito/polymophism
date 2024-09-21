9ba78b266868122764e04455e51d2109096fd60d
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-21 22:11:10 +0300

    Polymorphism with Duck Typing

---
+++ polymorphism DuckTyping.py
@@ -0,0 +1,5 @@
+# Polymorphism with Duck Typing
+# In Python, we can use duck typing to achieve polymorphism. Duck typing is a concept where
+# the suitability of an object is determined by the presence of certain methods or properties,
+# rather than the actual type of the object. This means that we can write code that works with
+# objects of different classes, as long as they have the required methods or properties.
